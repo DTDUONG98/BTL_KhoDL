065ac96af868a0cbfd8686c5af5d8512bfc342b6
vanhuy2612 <vanhuy2612@gmail.com>
2020-07-08 20:49:35 +0700

    no message

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,7 +10,7 @@ router.post('/', async (req, res, next) => {
         // make sure that any items are correctly URL encoded in the connection string
         await sql.connect(`mssql://sa:12345678@${IP}/DW`)
         let query = `
-            SELECT mh.Ma_MH as Ma_MH, SUM(f1.SoLuongDat) as SoLuongDat, SUM(f1.TongTien) as TongTien, SUM(f2.SoLuong) as SoLuong
+            SELECT DISTINCT mh.Ma_MH as Ma_MH, SUM(f1.SoLuongDat) as SoLuongDat, SUM(f1.TongTien) as TongTien, SUM(f2.SoLuong) as SoLuong
                 FROM Mathang as mh
                 INNER JOIN Fact1 as f1 ON mh.Ma_MH = f1.Ma_MH
                 INNER JOIN Fact2 as f2 ON mh.Ma_MH = f2.Ma_MH
@@ -117,21 +117,95 @@ router.post('/4', async (req, res, next) => {
 /*
 5. Với mỗi một đơn đặt hàng của khách, liệt kê các mặt hàng được đặt cùng với mô tả, mã cửa hàng, tên thành phố và các cửa hàng có bán mặt hàng đó.
 */
-
+router.post('/5', async (req, res, next) => {
+    try {
+        let Ma_Don = req.body.Ma_Don
+        // make sure that any items are correctly URL encoded in the connection string
+        await sql.connect(`mssql://sa:12345678@${IP}/DW`)
+        const result = await sql.query`
+        SELECT DISTINCT mh.Ma_MH AS Ma_MH, mh.MoTa AS MoTa,ch.Ma_CH AS Ma_CH, vp.Ten_TP AS Ten_TP
+            FROM MatHang AS mh 
+            INNER JOIN Fact1 AS f1 ON f1.Ma_MH = mh.Ma_MH AND f1.Ma_Don = ${Ma_Don}
+            INNER JOIN CuaHang AS ch ON  ch.Ma_CH = f1.Ma_CH
+            INNER JOIN VanPhongDD AS vp ON vp.Ma_TP = ch.Ma_TP       
+            
+        `
+        res.json(result.recordset)
+    } catch (err) {
+        // ... error checks
+    }
+})
 /*
 6. Tìm thành phố và bang mà một khách hàng nào đó sinh sống
 */
-
+router.post('/6', async (req, res, next) => {
+    try {
+        let Ma_KH = req.body.Ma_KH
+        // make sure that any items are correctly URL encoded in the connection string
+        await sql.connect(`mssql://sa:12345678@${IP}/DW`)
+        const result = await sql.query`
+        SELECT DISTINCT vp.Ma_TP AS Ma_TP, vp.Ten_TP AS Ten_TP, vp.Bang AS Bang 
+            FROM VanPhongDD AS vp 
+            INNER JOIN KhachHang AS kh ON kh.Ma_TP = vp.Ma_TP AND kh.Ma_KH = ${Ma_KH}
+        `
+        res.json(result.recordset)
+    } catch (err) {
+        // ... error checks
+    }
+})
 /*
 7. Tìm mức độ tồn kho của một mặt hàng cụ thể tại tất cả các cửa hàng ở một thành phố cụ thể nào đó
 */
+router.post('/7', async (req, res, next) => {
+    try {
+        let Ma_KH = req.body.Ma_KH
+        // make sure that any items are correctly URL encoded in the connection string
+        await sql.connect(`mssql://sa:12345678@${IP}/DW`)
+        const result = await sql.query`
 
+        `
+        res.json(result.recordset)
+    } catch (err) {
+        // ... error checks
+    }
+})
 /*
 8. Tìm các mặt hàng, số lượng đặt, khách hàng, cửa hàng và thành phố của một đơn đặt hàng.
 */
-
+router.post('/8', async (req, res, next) => {
+    try {
+        let Ma_Don = req.body.Ma_Don
+        // make sure that any items are correctly URL encoded in the connection string
+        await sql.connect(`mssql://sa:12345678@${IP}/DW`)
+        const result = await sql.query`
+        SELECT DISTINCT mh.Ma_MH AS Ma_MH, f1.SoLuongDat AS SoLuongDat, kh.TenKH AS TenKH, ch.Ma_CH AS Ma_CH, vp.Ten_TP AS Ten_TP 
+            FROM Fact1 AS f1
+            INNER JOIN CuaHang AS ch ON ch.Ma_CH = f1.Ma_CH
+            INNER JOIN MatHang AS mn ON mn.Ma_MH = f1.Ma_MH
+            INNER JOIN MatHang AS mh ON mh.Ma_MH = f1.Ma_MH
+            INNER JOIN KhachHang AS kh ON kh.Ma_KH = f1.Ma_KH
+            INNER JOIN VanPhongDD AS vp ON vp.Ma_TP = ch.Ma_TP
+	    WHERE f1.Ma_Don = ${Ma_Don}
+        `
+        res.json(result.recordset)
+    } catch (err) {
+        // ... error checks
+    }
+})
 /*
 9. Tìm các khách hàng du lịch, khách hàng đặt theo đường bưu điện và khách hàng thuộc cả hai loại
 */
-
+router.post('/9', async (req, res, next) => {
+    try {
+        let Ma_Don = req.body.Ma_Don
+        // make sure that any items are correctly URL encoded in the connection string
+        await sql.connect(`mssql://sa:12345678@${IP}/DW`)
+        const result = await sql.query`
+            SELECT * FROM KhachHang
+        `
+        res.json(result.recordset)
+    } catch (err) {
+        // ... error checks
+    }
+})
 module.exports = router
