ebd3242659425e31dc4e450f8b75f8cfd6327219
vanhuy2612 <vanhuy2612@gmail.com>
2020-07-08 17:56:38 +0700

    no message

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -37,7 +37,7 @@ router.post('/1', async (req, res, next) => {
         // make sure that any items are correctly URL encoded in the connection string
         await sql.connect('mssql://sa:12345678@localhost/DW')
         const result = await sql.query`
-        SELECT ch.Ma_CH as Ma_CH, ch.SoDienThoai as SoDienThoai, vp.Ten_TP as Ten_TP, vp.Bang as Bang,mh.Ma_MH AS Ma_MH, mh.MoTa as MoTa, mh.KichCo as KichCo, mh.Gia as Gia, mh.TrongLuong as TrongLuong  
+        SELECT DISTINCT ch.Ma_CH as Ma_CH, ch.SoDienThoai as SoDienThoai, vp.Ten_TP as Ten_TP, vp.Bang as Bang,mh.Ma_MH AS Ma_MH, mh.MoTa as MoTa, mh.KichCo as KichCo, mh.Gia as Gia, mh.TrongLuong as TrongLuong  
             FROM CuaHang as ch
             INNER JOIN Fact2 as f2 ON ch.Ma_CH = f2.Ma_CH 
             INNER JOIN VanPhongDD as vp ON f2.Ma_TP = vp.Ma_TP
@@ -52,15 +52,65 @@ router.post('/1', async (req, res, next) => {
 /*
 2. Tìm tất cả các đơn đặt hàng với tên khách hàng và ngày đặt hàng được thực hiện bởi khách hàng đó
 */
+router.post('/2', async (req, res, next) => {
+    try {
+        let TypeOfTime = req.body.TypeOfTime
+        // make sure that any items are correctly URL encoded in the connection string
+        await sql.connect('mssql://sa:12345678@localhost/DW')
+        const result = await sql.query`
+        SELECT DISTINCT d.Ma_Don AS Ma_Don,kh.Ma_KH AS Ma_KH, kh.TenKH AS TenKH, d.NgayDatHang AS NgayDatHang
+            FROM DonDatHang AS d
+            INNER JOIN Fact1 AS f1 ON d.Ma_Don = f1.Ma_Don
+            INNER JOIN KhachHang AS kh ON kh.Ma_KH = f1.Ma_KH
+            
+        `
+        res.json(result.recordset)
+    } catch (err) {
+        // ... error checks
+    }
+})
 /* 
 3. Tìm tất cả các cửa hàng cùng với tên thành phố và số điện thoại mà có bán các mặt hàng 
     được đặt bởi một khách hàng nào đó
 */
-
+router.post('/3', async (req, res, next) => {
+    try {
+        let TypeOfTime = req.body.TypeOfTime
+        // make sure that any items are correctly URL encoded in the connection string
+        await sql.connect('mssql://sa:12345678@localhost/DW')
+        const result = await sql.query`
+        SELECT DISTINCT ch.Ma_CH as Ma_CH, ch.SoDienThoai as SoDienThoai, vp.Ten_TP as Ten_TP, vp.Bang as Bang
+            FROM CuaHang AS ch
+            INNER JOIN Fact2 AS f2 ON f2.Ma_CH = ch.Ma_CH
+            INNER JOIN VanPhongDD AS vp ON vp.Ma_TP = f2.Ma_TP
+            INNER JOIN Fact1 AS f1 ON f1.Ma_CH = ch.Ma_CH 
+        WHERE f1.Ma_Don IS NOT NULL AND f1.Ma_KH IS NOT NULL AND f1.Ma_MH IS NOT NULL   
+        `
+        res.json(result.recordset)
+    } catch (err) {
+        // ... error checks
+    }
+})
 /*
 4. Tìm địa chỉ văn phòng đại diện với tên thành phố, bang của tất cả các cửa hàng lưu kho một mặt hàng nào đó với số lượng trên mức cụ thể.
 */
-
+router.post('/4', async (req, res, next) => {
+    try {
+        let TypeOfTime = req.body.TypeOfTime
+        // make sure that any items are correctly URL encoded in the connection string
+        await sql.connect('mssql://sa:12345678@localhost/DW')
+        const result = await sql.query`
+        SELECT d.Ma_Don AS Ma_Don,kh.Ma_KH AS Ma_KH, kh.TenKH AS TenKH, d.NgayDatHang AS NgayDatHang
+            FROM DonDatHang AS d
+            INNER JOIN Fact1 AS f1 ON d.Ma_Don = f1.Ma_Don
+            INNER JOIN KhachHang AS kh ON kh.Ma_KH = f1.Ma_KH
+            
+        `
+        res.json(result.recordset)
+    } catch (err) {
+        // ... error checks
+    }
+})
 /*
 5. Với mỗi một đơn đặt hàng của khách, liệt kê các mặt hàng được đặt cùng với mô tả, mã cửa hàng, tên thành phố và các cửa hàng có bán mặt hàng đó.
 */
